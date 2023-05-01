3045c325024ca2186dadc81e20e55007e2dac197
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2023-05-01 21:05:20 +0300

    Update BuyTokenViewController.swift

--- Application/Application/Screens/Operations/BuyToken/BuyTokenViewController.swift
+++ Application/Application/Screens/Operations/BuyToken/BuyTokenViewController.swift
@@ -12,6 +12,7 @@ class BuyTokenViewController: UIViewController {
     // MARK: - Properties.
     
     private let infoLabel = UILabel()
+    private let textField = UITextField()
     private let buyButton = UIButton(type: .system)
     
     // MARK: - viewDidLoad function.
@@ -27,8 +28,10 @@ class BuyTokenViewController: UIViewController {
         super.viewWillAppear(animated)
         if traitCollection.userInterfaceStyle == .light {
             view.backgroundColor = .secondarySystemBackground
+            textField.backgroundColor = .systemBackground
         } else {
             view.backgroundColor = .systemBackground
+            textField.backgroundColor = .secondarySystemBackground
         }
     }
     
@@ -38,8 +41,10 @@ class BuyTokenViewController: UIViewController {
         super.traitCollectionDidChange(previousTraitCollection)
         if traitCollection.userInterfaceStyle == .light {
             view.backgroundColor = .secondarySystemBackground
+            textField.backgroundColor = .systemBackground
         } else {
             view.backgroundColor = .systemBackground
+            textField.backgroundColor = .secondarySystemBackground
         }
     }
     
@@ -47,8 +52,11 @@ class BuyTokenViewController: UIViewController {
     
     private func setupViews() {
         setupInfoLabel()
+        setupTextField()
         setupBuyButton()
     }
+    
+    // MARK: - setupInfoLabel function.
     
     private func setupInfoLabel() {
         view.addSubview(infoLabel)
@@ -62,9 +70,41 @@ class BuyTokenViewController: UIViewController {
         infoLabel.font = .systemFont(ofSize: 18, weight: .regular)
     }
     
+    // MARK: - setupTextField function.
+    
+    private func setupTextField() {
+        view.addSubview(textField)
+        textField.pinTop(to: infoLabel.bottomAnchor, 16)
+        textField.pinLeft(to: view.leadingAnchor, 16)
+        textField.pinRight(to: view.trailingAnchor, 16)
+        textField.setHeight(to: 50)
+        let paddingView = UIView()
+        let textLabel = UILabel()
+        textLabel.setWidth(to: 90)
+        textLabel.setHeight(to: 40)
+        paddingView.addSubview(textLabel)
+        textLabel.pinTop(to: paddingView, 5)
+        textLabel.pinBottom(to: paddingView, 5)
+        textLabel.pinLeft(to: paddingView, 10)
+        textLabel.pinRight(to: paddingView, 5)
+        textLabel.text = "Amount:"
+        textLabel.textAlignment = .left
+        textLabel.textColor = .label
+        textLabel.font = .systemFont(ofSize: 18, weight: .medium)
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        textField.backgroundColor = .systemBackground
+        textField.layer.cornerRadius = 10
+        textField.keyboardType = .asciiCapableNumberPad
+        textField.returnKeyType = UIReturnKeyType.done
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing
+    }
+    
+    // MARK: - setupBuyButton function.
+    
     private func setupBuyButton() {
         view.addSubview(buyButton)
-        buyButton.pinTop(to: infoLabel.bottomAnchor, 10)
+        buyButton.pinTop(to: textField.bottomAnchor, 10)
         buyButton.pinLeft(to: view.safeAreaLayoutGuide.leadingAnchor, 16)
         buyButton.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 16)
         buyButton.setHeight(to: 50)
@@ -73,6 +113,8 @@ class BuyTokenViewController: UIViewController {
         buyButton.configuration?.cornerStyle = .medium
         buyButton.addTarget(self, action: #selector(buyButtonPressed), for: .touchUpInside)
     }
+    
+    // MARK: - buyButtonPressed function.
     
     @objc
     private func buyButtonPressed() {
